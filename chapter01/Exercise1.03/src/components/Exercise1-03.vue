0c0cbeed3d525189fbed9515405b1e590cbf23eb
tqndev <uznaruto298@gmail.com>
2025-06-10 16:57:56 +0700

    page 27

--- chapter01/Exercise1.03/src/components/Exercise1-03.vue
+++ chapter01/Exercise1.03/src/components/Exercise1-03.vue
@@ -2,9 +2,9 @@
   <div>
     <h1 v-if="true" v-once v-text="text"></h1>
     <h2 v-html="html" v-else-if="false"></h2>
-    <a :href="link.url" :target="link.target" :tabindex="link.tabindex">{{
-      link.title
-    }}</a>
+    <a :href="link.url" :target="link.target" :tabindex="link.tabindex">
+      {{ link.title }}
+    </a>
   </div>
 </template>
 <script setup>
