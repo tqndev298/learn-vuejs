779cd925105b6ed253b462c844341d46c12f0838
tqndev <uznaruto298@gmail.com>
2025-06-11 16:39:54 +0700

    100/626

---
+++ chapter01/Activity1.01/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Activity101 />
+</template>
+<script>
+import Activity101 from "./components/Activity1-01.vue";
+
+export default {
+  components: {
+    Activity101,
+  },
+};
+</script>
