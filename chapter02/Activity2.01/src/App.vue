507c92c5140560ae3b86e4306e857fbfa97490f9
tqndev <uznaruto298@gmail.com>
2025-06-12 14:38:14 +0700

    146/626

---
+++ chapter02/Activity2.01/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Activity201 />
+</template>
+
+<script>
+import Activity201 from "./components/Activity2-01.vue";
+
+export default {
+  components: {
+    Activity201,
+  },
+};
+</script>
